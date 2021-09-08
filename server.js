fa991cbf28f9aceecfdd11a5a47c2f8190d44958
sidd-iscoding <mailsiddharthbhuyan@gmail.com>
2021-09-08 08:07:07 +0530

    port edited

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express= require('express');  // import express module 
+const dotenv = require('dotenv');
+
+//Load env vars
+dotenv.config('config.env');
 const app=express();
 
 app.use(express.static('public'));
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 app.listen(port,function(){
     console.log(`listening at port ${port}`);
 });
